dfe6d6c8f1817b9ca4a0de2298b565aaab0f9821
young <randomyoung@users.noreply.github.com>
2016-11-29 21:35:57 -0500

    Initial Board code

---
+++ board.py
@@ -0,0 +1,24 @@
+import paillier
+
+class Board:
+    def __init__(self):
+        pass
+
+    # Is the sum of the votes 1
+    def validateRowTotal(self):
+        pass
+
+    # Is the vote in the set {0, 1}?
+    # Has the vote been tampered with?
+    # Using ZKP
+    def validateVote(self):
+        pass
+
+    def addVoteToTable(self):
+        pass
+
+def generateKeys():
+    pass
+
+if __name__ == "__main__":
+    pass
