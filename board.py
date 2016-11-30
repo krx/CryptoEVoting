7606201b83bd5bda091b2a69f2aea441b1f86c0b
Kareem El-Faramawi <kareem02@gmail.com>
2016-11-30 01:37:23 -0500

    Create a template command-based handler, use it for Board
    This should make it easy to map commands to functions

--- board.py
+++ board.py
@@ -1,24 +1,74 @@
+#!/usr/bin/env python2
+
+import thread
+from SocketServer import ThreadingTCPServer
+
 import paillier
+from common import *
 
-class Board:
-    def __init__(self):
-        pass
+# Paillier keys
+pub = None  # type: paillier.PublicKey
+priv = None  # type: paillier.PrivateKey
 
-    # Is the sum of the votes 1
-    def validateRowTotal(self):
-        pass
+# Table containing vote results
+board = {}
 
-    # Is the vote in the set {0, 1}?
-    # Has the vote been tampered with?
-    # Using ZKP
-    def validateVote(self):
-        pass
 
-    def addVoteToTable(self):
-        pass
+# class Board:
+#     def __init__(self):
+#         pass
+#
+#     # Is the sum of the votes 1
+#     def validateRowTotal(self):
+#         pass
+#
+#     # Is the vote in the set {0, 1}?
+#     # Has the vote been tampered with?
+#     # Using ZKP
+#     def validateVote(self):
+#         pass
+#
+#     def addVoteToTable(self):
+#         pass
 
-def generateKeys():
-    pass
+
+class BoardHandler(CommandHandler):
+    reg_open = False
+
+    def init_commands(self):
+        # Is registration currently allowed?
+        self.commands['regopen'] = lambda _: self.reg_open
+
+        # Get the public key (modulus)
+        self.commands['pubkey'] = lambda _: pub.n
+
+        self.commands['vote'] = self.attempt_vote
+
+    def attempt_vote(self, args):
+        if self.reg_open:
+            # Not allowed to vote yet
+            return
+
+        print args
+
 
 if __name__ == "__main__":
-    pass
+    # Generate a new set of keys
+    print 'Generating Paillier key pair ...',
+    pub, priv = paillier.gen_keypair()
+    print 'done!'
+
+    print 'Starting board server at {}:{}'.format(HOST, PORT_BOARD)
+    ThreadingTCPServer.allow_reuse_address = True
+    srv = ThreadingTCPServer((HOST, PORT_BOARD), BoardHandler)
+    thread.start_new_thread(srv.serve_forever, ())
+
+    # There are two "phases" before shutting down
+    BoardHandler.reg_open = True
+    raw_input('--- BEGIN REGISTRATION PHASE ---')
+
+    BoardHandler.reg_open = False
+    raw_input('--- BEGIN VOTING PHASE ---')
+
+    print '--- VOTING COMPLETE, COUNTING VOTES ---'
+    # do counting stuff here
--- common.py
+++ common.py
@@ -1,6 +1,42 @@
 """
-Common variables shared between services
+Common variables/utilities shared between services
 """
+import json
+from SocketServer import StreamRequestHandler
+
 HOST = 'localhost'
 PORT_REGISTRAR = 9001
 PORT_BOARD = 9002
+
+
+class CommandHandler(StreamRequestHandler):
+    def __init__(self, request, client_address, server):
+        self.commands = {}
+        self.init_commands()
+        StreamRequestHandler.__init__(self, request, client_address, server)
+
+    def init_commands(self):
+        raise NotImplementedError('Define commands for this handler')
+
+    def println(self, s=''):
+        self.wfile.write('{}\n'.format(s))
+
+    def input(self, prompt=''):
+        self.wfile.write(prompt)
+        return self.rfile.readline().rstrip('\r\n')
+
+    def handle(self):
+        try:
+            cmd = json.loads(self.rfile.readline())
+            assert 'command' in cmd and 'args' in cmd
+            res = self.commands[cmd['command']](cmd['args'])
+            self.println(json.dumps({'res': res}))
+        except:
+            self.println('Invalid input')
+
+
+def make_cmd(cmd, args=None):
+    return json.dumps({
+        'command': cmd.lower(),
+        'args': args or {}
+    })
