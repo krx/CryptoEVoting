9549242cf1b9d049214a6c2b87caf2a49a37f0f3
negasora <negasora@gmail.com>
2016-11-29 21:47:06 -0500

    made registrar threaded n stuff

--- registrar.py
+++ registrar.py
@@ -1,55 +1,65 @@
-import SocketServer, hashlib, sqlite3
+import SocketServer, hashlib, sqlite3, threading
 
 voterdb = sqlite3.connect("voters.db")
 cursor = voterdb.cursor()
+
 try:
     cursor.execute("create table voters (name text, password text)")
-
     voterdb.commit()
 except:
     pass
 
+voterdb.close()
 
 class VoterHandler(SocketServer.StreamRequestHandler):
-
+    def sql(self, query, args=None):
+        if args:
+            return self.cursor.execute(query, args)
+        else:
+            return self.cursor.execute(query)
     def salt(self, password):
         return 'whyso' + password + 'salty'
 
     def register(self, name, password):
         
-        if cursor.execute("select name from voters where name=?",(name,)).fetchone() != None:
+        if self.sql("select name from voters where name=?",(name,)).fetchone() != None:
             return "Name already registered\n"
         
         voterinfo = (name, hashlib.sha256(self.salt(password)).hexdigest())
-        cursor.execute("insert into voters values (?,?)", voterinfo)
-        voterdb.commit()
+        print voterinfo
+        self.sql("insert into voters values (?,?)", voterinfo)
+        self.voterdb.commit()
 
-        #for row in cursor.execute("select name,password from voters where 1=1"):
-        #    print row
+        for row in self.sql("select name,password from voters where 1=1"):
+            print row
 
         return "Successfully registered"
 
 
     def handle(self):
-        
+        self.voterdb = sqlite3.connect("voters.db")
+        self.cursor = self.voterdb.cursor()
+    
         self.data = self.rfile.readline().strip()
         
         if "REGISTER " in self.data:
             info = self.data.split(' ')
-            self.wfile.write(self.register(info[1], info[2]))
+            try:
+                self.wfile.write(self.register(info[1], info[2]))
+            except:
+                self.wfile.write("REGISTER [username] [password]")
         
         else:
-            self.wfile.write("not implemented yet")
+            self.wfile.write("Options: REGISTER")
 
 
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
 
 HOST, PORT = "localhost", 1337
 
-# Create the server, binding to localhost on port 9999
-
 SocketServer.TCPServer.allow_reuse_address = True
-server = SocketServer.TCPServer((HOST, PORT), VoterHandler)
+server = ThreadedTCPServer((HOST, PORT), VoterHandler)
 
-# Activate the server; this will keep running until you
-# interrupt the program with Ctrl-C
 server.serve_forever()
+
