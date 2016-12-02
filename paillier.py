776626ec49d7636213a4e3c1d8eabd61d8831a22
Kareem El-Faramawi <kareem02@gmail.com>
2016-12-02 03:38:41 -0500

    paillier docs

--- paillier.py
+++ paillier.py
@@ -7,12 +7,24 @@ from Crypto.Util.number import inverse, getStrongPrime, getRandomRange, GCD
 
 
 class PublicKey:
+    """
+    Contains a Paillier public key (modulus, generator) and allows for encryption
+    """
+
     def __init__(self, n):
         self.n = n
-        self.n_sq = n * n
+        self.n_sq = n * n  # Just for convenience / no need to recalculate
         self.g = n + 1  # Works because p, q are the same size
 
     def encrypt(self, ptxt):
+        """ Encrypts the given plaintext using this public key
+
+        Args:
+            ptxt (long): The numeric representation of the plaintext to be encrypted
+
+        Returns:
+            EncryptedMessage: The resulting encryption
+        """
         if not isinstance(ptxt, (int, long)):
             raise TypeError('Plaintext must be a number')
 
@@ -30,12 +42,24 @@ class PublicKey:
 
 
 class PrivateKey:
+    """
+    Contains all key components for Paillier encryption/decryption
+    """
+
     def __init__(self, pub, p, q):
         self.pub = pub
         self.lm = (p - 1) * (q - 1)
         self.mu = inverse(self.lm, pub.n)
 
     def decrypt(self, c):
+        """ Decrypts the given ciphertext using this private key
+
+        Args:
+            c (long, EncryptedMessage): The ciphertext to be decrypted
+
+        Returns:
+            long: Numeric representation of the decrypted plaintext
+        """
         if isinstance(c, EncryptedMessage):
             if c.pub != self.pub:
                 raise ValueError("Public keys don't match")
@@ -48,12 +72,26 @@ class PrivateKey:
 
 
 class EncryptedMessage:
+    """
+    Wrapper around a Paillier ciphertext to add more functionality
+    """
+
     def __init__(self, pub, ctxt, r=None):
         self.pub = pub
         self.ctxt = ctxt
-        self.rand_num = r
+        self.rand_num = r  # Keeps track of the random number used for encryption (for ZKP)
 
     def __add__(self, other):
+        """ Adds another encrypted message or plaintext to this one
+
+        Args:
+            other (long, EncryptedMessage): The value to be added to this one
+
+        Returns:
+            EncryptedMessage: If another encrypted message is given, the result will
+            decrypt to the sum of the two decrypted plaintext. If given a scalar value,
+            the result will decrypt to the sum of this plaintext and the given value.
+        """
         add_val = 1
         if isinstance(other, EncryptedMessage):
             if self.pub != other.pub:
@@ -73,6 +111,16 @@ class EncryptedMessage:
         return self + -other
 
     def __mul__(self, other):
+        """ Multiplies this encrypted text by a scalar value
+
+        Args:
+            other (long): Value to mutliply this encrypted message by
+
+        Returns:
+            EncryptedMessage: An encrypted message that decrypts to this plaintext
+            multiplied by the given scalar value
+
+        """
         if not isinstance(other, (int, long)):
             raise TypeError('Encrypted message must be multiplied by a number')
 
@@ -102,6 +150,18 @@ def gen_keypair(nbits=2048):
 
 
 def random_range_coprime(a, b, n):
+    """
+    Generates a random value r in the range a <= r < b, with r being coprime to n
+    (aka Z star)
+
+    Args:
+        a (long): Lower bound
+        b (long): Upper bound
+        n (long): Modulus
+
+    Returns:
+        A random value r in the given range such that GCD(r, n) == 1
+    """
     res = getRandomRange(a, b)
     while GCD(res, n) != 1:
         res = getRandomRange(a, b)
